8c7eaecb7630add1096c5cdeb07864c902efdb1c
Fábián Gábor <info@fabiangabor.com>
2021-02-16 16:21:23 +0100

    Gyak2 példák

---
+++ Gyak2/Gyak2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyak2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 32;
+            double y = 32;
+
+            Console.WriteLine("x.ToString(): " + x.ToString());
+            Console.WriteLine("x.Equals(y): " + x.Equals(y));
+            Console.WriteLine("y.Equals(x): " + y.Equals(x));
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+            // int z = null; // igy nem
+            Nullable<int> z = null;
+            // int ? z = null; // igy is lehet
+            if (z.HasValue)
+            {
+                Console.WriteLine("Van erteke: " + z.Value);
+            } else
+            {
+                Console.WriteLine("Nincs erteke.");
+            }
+        }
+    }
+}
